139ab0564db20e755b24299a6dd8997b814d1c44
Francisco Machado Becker <franciscombecker@gmail.com>
2025-01-30 16:10:08 -0300

    feat: novas propriedades da classe user_data

--- app/schema.py
+++ app/schema.py
@@ -1,6 +1,10 @@
+from typing import Optional
 from pydantic import BaseModel
 
 
 class UserData(BaseModel):
     email: str
     password: str
+    display_name: Optional[str] = None
+    phone_number: Optional[str] = None
+    photo_url: Optional[str] = None
