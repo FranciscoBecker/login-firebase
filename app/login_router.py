038b4bf207ac8322eb53eb0311e8cbd82ac408ec
Francisco Machado Becker <franciscombecker@gmail.com>
2025-01-30 16:10:33 -0300

    feat: alteração para permitir cadastrar usuário com nome, telefone e foto

--- app/login_router.py
+++ app/login_router.py
@@ -1,37 +1,54 @@
 import pyrebase
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException
 from schema import UserData
 import json
+from firebase_admin import auth
 
 ########################################################
 # ROTAS PARA CADASTRAR UM USUÁRIO E PARA FAZER LOGIN   #
 # EM UM PROJETO REAL, ESSAS FUNÇÕES FICAM NO FRONT END #
 ########################################################
 
+# CONFIGURAÇÃO DO PYREBASE ------------------------------------------------------------------------#
 with open("C:/Users/W027224/Documents/login-firebase/resources/firebaseConfig.json", "r") as f:
     config = json.load(f)
 
-firebase = pyrebase.initialize_app(config)
-auth = firebase.auth()
+pyrebase = pyrebase.initialize_app(config)
+pyrebase_auth = pyrebase.auth()
+# -------------------------------------------------------------------------------------------------#
 
 login_router = APIRouter()
+
 
 @login_router.post("/login")
 def login_route(user_data: UserData):
     try:
-        login = auth.sign_in_with_email_and_password(user_data.email, user_data.password)
+        login = pyrebase_auth.sign_in_with_email_and_password(user_data.email, user_data.password)
         print(login)
         return login
     except Exception as e:
         print(e)
-    return
+        raise HTTPException(status_code=500, detail=str(e))
+
 
 @login_router.post("/signup")
 def signup(user_data: UserData):
     try:
-        user = auth.create_user_with_email_and_password(user_data.email, user_data.password)
+        # CRIANDO USUÁRIO COM O PYREBASE
+        # user = pyrebase_auth.create_user_with_email_and_password(user_data.email, user_data.password)
+        # CRIANDO USUÁRIO COM O FIREBASE_ADMIN
+        user = auth.create_user(
+            email=user_data.email,
+            email_verified=False,
+            phone_number=user_data.phone_number,
+            password=user_data.password,
+            display_name=user_data.display_name,
+            photo_url=user_data.photo_url,
+            disabled=False
+        )
+
         print(user)
         return user
     except Exception as e:
         print(e)
-    return
+        raise HTTPException(status_code=500, detail=str(e))
