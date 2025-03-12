d0984b49d21c3f675d2f0447cee0aa0237efada1
Francisco Machado Becker <franciscombecker@gmail.com>
2025-03-12 10:51:18 -0300

    feat: adicionada configuração para rodar o projeto chamando o aquivo main

--- app/main.py
+++ app/main.py
@@ -1,7 +1,12 @@
 from fastapi import FastAPI
 import login_router, router
+import uvicorn
 
 app = FastAPI()
 
 app.include_router(router.router)
 app.include_router(login_router.login_router)
+
+if __name__ == '__main__':
+    uvicorn.run(app, reload=True, host='localhost',
+                port=8000, log_level='debug', workers=1)
